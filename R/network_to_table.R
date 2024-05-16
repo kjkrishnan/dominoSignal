8a75f8844ce94ed682171936285f36c199f15a91
kjkrishnan <kkrish12@jhmi.edu>
2024-05-16 16:24:28 -0400

    update network_to_df function to fix bugs and use list rather than df for faster performance

--- R/network_to_table.R
+++ R/network_to_table.R
@@ -1,8 +1,9 @@
 # For additional helper functions that might prove useful in any domino2 analysis
-# Goal: extract network connections (and their values) from domino object to data frame
+# (with considerable assistance from Sushma and others from the Fertig lab)
 
 require(purrr)
 require(tidyverse)
+require(Matrix)
 
 #' Convert between ligand names and gene names
 #' @param dom A built domino object
@@ -16,13 +17,16 @@ resolve_names <- function(dom, genes) {
     genes_resolved <- sapply(genes, function(l) {
         # For each ligand, find the first row in the rl_map
         int <- rl_map[rl_map$L.name == l, ][1, ]
-        # If the ligand name and ligand gene aren't the same,
+        # If the ligand is in the rl_map and ligand name and ligand gene aren't the same,
         # and there is no comma in the ligand gene, use the gene
-        if ((int$L.name != int$L.gene) & !grepl("\\,", int$L.gene)) {
+        if ((l %in% rl_map$L.name) & (int$L.name != int$L.gene) & !grepl("\\,", int$L.gene)) {
             remove_semicolons(int$L.gene)
             # Otherwise, use the ligand name
-        } else {
+        } else if (l %in% rl_map$L.name) {
             remove_semicolons(int$L.name)
+            # If the ligand is not in the rl_map, just return ligand without semicolons
+        } else {
+            remove_semicolons(l)
         }
     })
     return(genes_resolved)
@@ -39,11 +43,11 @@ resolve_complexes <- function(dom, genes) {
         # If ligand is in a complex
         if (length(l) > 1) {
             # Just check for semis and return? Why is this happening?
-            check_semis <- sapply(X = l, FUN = remove_semicolons)
+            check_semis <- sapply(X = l, FUN = remove_semicolons, USE.NAMES = FALSE)
         } else if (l %in% names(dom@linkages$complexes)) {
             # Return the genes in the complex (after checking for semicolons)
             subunits = dom@linkages$complexes[[l]]
-            check_semis <- sapply(X = subunits, FUN = remove_semicolons)
+            check_semis <- sapply(X = subunits, FUN = remove_semicolons, USE.NAMES = FALSE)
             # Otherwise just return the ligand (after checking for semicolons)
         } else {
             check_semis <- remove_semicolons(l)
@@ -83,7 +87,7 @@ avg_exp_for_complexes <- function(exp_mat, complexes_list) {
 
 #' Get ligands with resolved names
 #' @param dom A built domino object
-#' @return A vector of ligands with resolved names
+#' @return A list of ligands and complexes with resolved names
 #' @keywords internal
 get_resolved_ligands <- function(dom) {
     all_lig <- unlist(dom@linkages$rec_lig)
@@ -104,7 +108,7 @@ get_resolved_ligands <- function(dom) {
 #' @param send_clusters A cluster name or vector for which the outgoing signal is desired
 #' @param lig_genes A vector of ligand genes
 #' @param complexes A list of complexes with names as complex and values as component genes
-#' @param matrix A character vector of length 1, either "counts" or "z_scores"
+#' @param exp_type A character vector of length 1, either "counts" or "z_scores"
 #' @return A matrix of ligand expression for outgoing clusters
 #' @keywords internal
 get_ligand_expression <- function(dom, send_clusters, lig_genes, complexes, exp_type) {
@@ -153,7 +157,8 @@ get_ligand_expression <- function(dom, send_clusters, lig_genes, complexes, exp_
 #' @return A data frame of signaling information
 #' @keywords internal
 get_signaling_info <- function(dom, rec_clusters, cl_ligands_sub, exp_type) {
-    df <- data.frame()
+    row_list <- list()
+    i = 1
     for (cl in rec_clusters) {
         for (tf in names(dom@linkages$clust_tf_rec[[cl]])) {
             recs <- dom@linkages$clust_tf_rec[[cl]][[tf]]
@@ -176,8 +181,8 @@ get_signaling_info <- function(dom, rec_clusters, cl_ligands_sub, exp_type) {
                             } else if (exp_type == "z_scores") {
                                 rec_sig <- mean(dom@z_scores[rec_sep, which(dom@clusters == cl)])
                             }
-                            tf_sig <- mean(dom@features[tf, which(dom@clusters == cl)])
                         }
+                        tf_sig <- mean(dom@features[tf, which(dom@clusters == cl)])
                     } else if (rec_cell == 1) {
                         if (length(rec_sep) > 1) {
                             if (exp_type == "counts") {
@@ -215,18 +220,19 @@ get_signaling_info <- function(dom, rec_clusters, cl_ligands_sub, exp_type) {
                                 sending_cl = df_temp$cluster,
                                 receiving_cl = cl
                             )
-                            df <- rbind(df, new_row)
+                            row_list[[i]] <- new_row
+                            i = i + 1
                         }
                     }
                 }
             }
         }
     }
-    
+    df = purrr::list_rbind(row_list)
     return(df)
 }
 
-#' Turn domino objecct signaling information into a data frame
+#' Turn domino object signaling information into a data frame
 #' 
 #' This function takes a domino object and returns a data frame of signaling information with
 #' columns for ligand, receptor, transcription factor, ligand expression, receptor expression,
